a822d66182ad0d8d87e12e5521c941c78a759040
reach2jeyan <iamsmj@gmail.com>
2019-07-08 23:45:28 +0530

    Added to open terminal directly

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const electron =require('electron');
 //for UI "app"
 const app=electron.app;
 const BrowerWindow=electron.BrowserWindow;
-
 const path=require('path');
 const url=require('url');
+const {Menu} = require('electron')
+
+
 
 let win; //reference to window
 
@@ -36,6 +38,75 @@ app.on('window-all-closed',()=>{
 app.on('activate',()=>{
     if(win==null){
         createWindow()
+        createMenu
     }
 });
 
+    // Create the Application's main menu
+    function createMenu() {
+        const application = {
+          label: "Application",
+          submenu: [
+            {
+              label: "About Application",
+              selector: "orderFrontStandardAboutPanel:"
+            },
+            {
+              type: "separator"
+            },
+            {
+              label: "Quit",
+              accelerator: "Command+Q",
+              click: () => {
+                app.quit()
+              }
+            }
+          ]
+        }
+      
+        const edit = {
+          label: "Edit",
+          submenu: [
+            {
+              label: "Undo",
+              accelerator: "CmdOrCtrl+Z",
+              selector: "undo:"
+            },
+            {
+              label: "Redo",
+              accelerator: "Shift+CmdOrCtrl+Z",
+              selector: "redo:"
+            },
+            {
+              type: "separator"
+            },
+            {
+              label: "Cut",
+              accelerator: "CmdOrCtrl+X",
+              selector: "cut:"
+            },
+            {
+              label: "Copy",
+              accelerator: "CmdOrCtrl+C",
+              selector: "copy:"
+            },
+            {
+              label: "Paste",
+              accelerator: "CmdOrCtrl+V",
+              selector: "paste:"
+            },
+            {
+              label: "Select All",
+              accelerator: "CmdOrCtrl+A",
+              selector: "selectAll:"
+            }
+          ]
+        }
+      
+        const template = [
+          application,
+          edit
+        ]
+      
+        Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+      }
